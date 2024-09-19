d048a178273d75b2ec7d293f8b2d7095e856ec94
DivyaMulchandani <113537199+DivyaMulchandani@users.noreply.github.com>
2024-09-19 22:54:39 +0530

    update

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -1,33 +1,26 @@
 const mongoose = require("mongoose");
 const inventoryModel = require("../models/inventoryModel");
 const userModel = require("../models/userModel");
-
+// CREATE INVENTORY
 const createInventoryController = async (req, res) => {
   try {
+    const { email } = req.body;
     //validation
-    const { email, inventoryType } = req.body;
-    const { name, description, price, quantity } = req.body;
-    const user = await userModel.findOne({
-      email: email,
-    });
+    const user = await userModel.findOne({ email });
     if (!user) {
-      return res.status(404).json({ message: "User not found" });
+      throw new Error("User Not Found");
     }
     // if (inventoryType === "in" && user.role !== "donar") {
-    //   res
-    //     .status(403)
-    //     .json({ message: "You are not authorized to add inventory" });
-    //   throw new Error("Not a Donar account."); ///a change done check here if error comes
+    //   throw new Error("Not a donar account");
     // }
     // if (inventoryType === "out" && user.role !== "hospital") {
     //   throw new Error("Not a hospital");
     // }
-
     if (req.body.inventoryType == "out") {
       const requestedBloodGroup = req.body.bloodGroup;
       const requestedQuantityOfBlood = req.body.quantity;
       const organisation = new mongoose.Types.ObjectId(req.body.userId);
-      //calculate Blood Quantity
+      //calculate Blood Quanitity
       const totalInOfRequestedBlood = await inventoryModel.aggregate([
         {
           $match: {
@@ -43,11 +36,10 @@ const createInventoryController = async (req, res) => {
           },
         },
       ]);
-      //console.log("Total in:", totalInOfRequestedBlood);
+      // console.log("Total In", totalInOfRequestedBlood);
       const totalIn = totalInOfRequestedBlood[0]?.total || 0;
-
-      //calculate out blood Quantity
-      const totalOutOfRequestedBlood = await inventoryModel.aggregate([
+      //calculate OUT Blood Quanitity
+      const totalOutOfRequestedBloodGroup = await inventoryModel.aggregate([
         {
           $match: {
             organisation,
@@ -62,62 +54,224 @@ const createInventoryController = async (req, res) => {
           },
         },
       ]);
-      //console.log("Total out:", totalOutOfRequestedBlood);
-      const totalOut = totalOutOfRequestedBlood[0]?.total || 0;
-
-      //in and out calc
-      const availableQuatityOfBloodGroup = totalIn - totalOut;
-
+      const totalOut = totalOutOfRequestedBloodGroup[0]?.total || 0;
+      //in & Out Calc
+      const availableQuanityOfBloodGroup = totalIn - totalOut;
       //quantity validation
-      if (availableQuatityOfBloodGroup < requestedQuantityOfBlood) {
+      if (availableQuanityOfBloodGroup < requestedQuantityOfBlood) {
         return res.status(500).send({
           success: false,
           message: `Only ${availableQuanityOfBloodGroup}ML of ${requestedBloodGroup.toUpperCase()} is available`,
         });
       }
       req.body.hospital = user?._id;
+    } else {
+      req.body.donar = user?._id;
     }
 
     //save record
     const inventory = new inventoryModel(req.body);
     await inventory.save();
-    res.status(201).json({
+    return res.status(201).send({
       success: true,
-      message: "Inventory created successfully",
+      message: "New Blood Reocrd Added",
     });
   } catch (error) {
     console.log(error);
     return res.status(500).send({
-      message: "Error creating inventory",
       success: false,
+      message: "Errro In Create Inventory API",
       error,
     });
   }
 };
-
-//GET ALL INVENTORY
+// GET ALL BLOOD RECORS
 const getInventoryController = async (req, res) => {
   try {
     const inventory = await inventoryModel
-      .find({
-        organisation: req.body.userId,
-      })
+      .find
+      //   {
+      //   organisation: req.body.userId,
+      // }
+      ()
       .populate("donar")
       .populate("hospital")
       .sort({ createdAt: -1 });
-    return res.status(200).json({
+    console.log("Total Inventory Records: ", inventory.length);
+
+    return res.status(200).send({
       success: true,
-      message: "Inventory found successfully",
+      messaage: "get all records successfully",
       inventory,
     });
   } catch (error) {
     console.log(error);
     return res.status(500).send({
-      message: "Error fetching inventory",
       success: false,
+      message: "Error In Get All Inventory",
       error,
     });
   }
 };
 
-module.exports = { createInventoryController, getInventoryController };
+// GET DONAR REOCRDS
+const getDonarsController = async (req, res) => {
+  try {
+    const organisation = req.body.userId;
+    //find donars
+    const donorId = await inventoryModel.distinct("donar", {
+      organisation,
+    });
+    // console.log(donorId);
+    const donars = await userModel.find({ _id: { $in: donorId } });
+
+    return res.status(200).send({
+      success: true,
+      message: "Donar Record Fetched Successfully",
+      donars,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      success: false,
+      message: "Error in Donar records",
+      error,
+    });
+  }
+};
+
+// GET DONAR REOCRDS
+const getDonarsControllerForHospital = async (req, res) => {
+  try {
+    const hospital = req.body.userId;
+    //find donars
+    const donorId = await inventoryModel.distinct("donar", {
+      hospital,
+    });
+    // console.log(donorId);
+    const donars = await userModel.find({ _id: { $in: donorId } });
+
+    return res.status(200).send({
+      success: true,
+      message: "Donar Record Fetched Successfully",
+      donars,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      success: false,
+      message: "Error in Donar records",
+      error,
+    });
+  }
+};
+
+//Get All Hospitals
+const getHospitalController = async (req, res) => {
+  try {
+    const organisation = req.body.userId;
+    //GET HOSPITAL ID
+    const hospitalId = await inventoryModel.distinct("hospital", {
+      organisation,
+    });
+    //FIND HOSPITAL
+    const hospitals = await userModel.find({
+      _id: { $in: hospitalId },
+    });
+    return res.status(200).send({
+      success: true,
+      message: "Hospitals Data Fetched Successfully",
+      hospitals,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      success: false,
+      message: "Error In get Hospital API",
+      error,
+    });
+  }
+};
+
+// GET ORG PROFILES
+const getOrgnaisationController = async (req, res) => {
+  try {
+    const donar = req.body.userId;
+    const orgId = await inventoryModel.distinct("organisation", { donar });
+    //find org
+    const organisations = await userModel.find({
+      _id: { $in: orgId },
+    });
+    return res.status(200).send({
+      success: true,
+      message: "Org Data Fetched Successfully",
+      organisations,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      success: false,
+      message: "Error In ORG API",
+      error,
+    });
+  }
+};
+
+// GET ORG for Hospital
+const getOrgnaisationForHospitalController = async (req, res) => {
+  try {
+    const hospital = req.body.userId;
+    const orgId = await inventoryModel.distinct("organisation", { hospital });
+    //find org
+    const organisations = await userModel.find({
+      _id: { $in: orgId },
+    });
+    return res.status(200).send({
+      success: true,
+      message: "Hospital Org Data Fetched Successfully",
+      organisations,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      success: false,
+      message: "Error In Hospital ORG API",
+      error,
+    });
+  }
+};
+
+// GET Hospital BLOOD RECORS
+const getInventoryHospitalController = async (req, res) => {
+  try {
+    const inventory = await inventoryModel
+      .find(req.body.filters)
+      .populate("donar")
+      .populate("hospital")
+      .populate("organisation")
+      .sort({ createdAt: -1 });
+    return res.status(200).send({
+      success: true,
+      messaage: "get hospital comsumer records successfully",
+      inventory,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      success: false,
+      message: "Error In Get consumer Inventory",
+      error,
+    });
+  }
+};
+
+module.exports = {
+  createInventoryController,
+  getInventoryController,
+  getDonarsController,
+  getHospitalController,
+  getOrgnaisationController,
+  getOrgnaisationForHospitalController,
+  getDonarsControllerForHospital,
+  getInventoryHospitalController,
+};
--- routes/inventoryRoutes.js
+++ routes/inventoryRoutes.js
@@ -3,6 +3,12 @@ const authMiddelware = require("../middlewares/authMiddelware");
 const {
   createInventoryController,
   getInventoryController,
+  getDonarsController,
+  getHospitalController,
+  getOrgnaisationController,
+  getOrgnaisationForHospitalController,
+  getDonarsControllerForHospital,
+  getInventoryHospitalController,
 } = require("../controllers/inventoryController");
 
 const router = express.Router();
@@ -13,5 +19,35 @@ router.post("/create-inventory", authMiddelware, createInventoryController);
 
 //GET ALL BLOOD RECORDS
 router.get("/get-inventory", authMiddelware, getInventoryController);
+
+//Get All Donars
+router.get("/get-donars", authMiddelware, getDonarsController);
+
+//Get All Donars
+router.get(
+  "/get-donars-for-hospital",
+  authMiddelware,
+  getDonarsControllerForHospital
+);
+
+//GET HOSPITAL RECORDS
+router.get("/get-hospitals", authMiddelware, getHospitalController);
+
+//GET orgnaisation RECORDS
+router.get("/get-orgnaisation", authMiddelware, getOrgnaisationController);
+
+//GET orgnaisation RECORDS
+router.get(
+  "/get-orgnaisation-for-hospital",
+  authMiddelware,
+  getOrgnaisationForHospitalController
+);
+
+//GET HOSPITAL BLOOD RECORDS
+router.post(
+  "/get-inventory-hospital",
+  authMiddelware,
+  getInventoryHospitalController
+);
 
 module.exports = router;
