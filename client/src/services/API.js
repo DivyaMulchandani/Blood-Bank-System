76e9e771c493e89006602df1d9c756d0f2ceb09a
DivyaMulchandani <113537199+DivyaMulchandani@users.noreply.github.com>
2024-09-22 16:50:38 +0530

    Update API.js

--- client/src/services/API.js
+++ client/src/services/API.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 // const API = axios.create({ baseURL: process.env.REACT_APP_BASEURL });
 
-const API = axios.create({ baseURL: "https://blood-bank-api.onrender.com" });
+const API = axios.create({
+  baseURL: "https://blood-bank-system-api.onrender.com",
+});
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("token")) {
