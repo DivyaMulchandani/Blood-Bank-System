a7e2531676f7f05a8e6abd885658fa9138128a75
DivyaMulchandani <113537199+DivyaMulchandani@users.noreply.github.com>
2024-09-22 16:09:47 +0530

    render 1st step

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -5,6 +5,7 @@ const store = configureStore({
   reducer: {
     auth: authSlice.reducer,
   },
+  devTools: false,
 });
 
 export default store;
--- client/src/services/API.js
+++ client/src/services/API.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: process.env.REACT_APP_BASEURL });
+// const API = axios.create({ baseURL: process.env.REACT_APP_BASEURL });
+
+const API = axios.create({ baseURL: "https://blood-bank-api.onrender.com" });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("token")) {
